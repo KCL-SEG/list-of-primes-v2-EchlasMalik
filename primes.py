23739851ff135c8f5cca87b960a9b364a19b41e7
Echlas Malik <99184338+EchlasMalik@users.noreply.github.com>
2022-12-09 10:54:24 +0000

    Slow Solution

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes<1):
+        raise ValueError("0 and negative numbers not accepted")
     list = []
+    number = 0
+    prime_number = 2
+    is_prime = True
+
+    while number < (number_of_primes):
+        for n in range(2,prime_number):
+            if(prime_number % n == 0):    #Checks if any number between 2 and itself divide. (Checking for any non-prime numbers)
+                is_prime = False
+            
+            elif is_true == True:
+                list.append(prime_number)
+                count = count + 1
+            
+            prime_number = prime_number + 1
     return list
